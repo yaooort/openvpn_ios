6ae63f68fc2212d08fe5ba63996424f1d2bcd631
Jorah <yang.li.gogle@gmail.com>
2018-06-23 01:00:49 +0700

    rm framework from main proj

--- jiyunSpeed/ViewController.swift
+++ jiyunSpeed/ViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var mBtn: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
