48efb53e188d470523bf5469a23d1bda668ffb24
Jorah <yang.li.gogle@gmail.com>
2018-06-22 22:18:17 +0700

    Initial Commit

---
+++ jiyunSpeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jiyunSpeed
+//
+//  Created by Jorah Li on 22/6/2561 BE.
+//  Copyright © 2561 Jorah Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
