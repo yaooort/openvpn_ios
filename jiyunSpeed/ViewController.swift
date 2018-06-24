1304bd4109b526d5304b9da1de9b95dff7381d44
Jorah <yang.li.gogle@gmail.com>
2018-06-24 18:41:01 +0700

    add vpn status watch

--- jiyunSpeed/ViewController.swift
+++ jiyunSpeed/ViewController.swift
@@ -9,14 +9,34 @@
 import UIKit
 import NetworkExtension
 
+public enum VPNStatus {
+    case Off
+    case Connecting
+    case On
+    case Disconnecting
+}
+
+
 class ViewController: UIViewController {
-
-    var manager: NETunnelProviderManager?
-
+    @IBOutlet weak var mVpnStatus: UILabel!
+    @IBOutlet weak var mConnectBtn: UIButton!
+    @IBOutlet weak var mStopBtn: UIButton!
+    private var manager: NETunnelProviderManager?
+    public var observerAdded: Bool = false
+    public let kProxyServiceVPNStatusNotification = "kProxyServiceVPNStatusNotification"
+    
+    
+    public private(set) var vpnStatus = VPNStatus.Off {
+        didSet {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: kProxyServiceVPNStatusNotification), object: nil)
+        }
+    }
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initBtn()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,9 +44,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    func initBtn () {
+        mConnectBtn.tag = 2000
+        mStopBtn.tag = 2001
+        mStopBtn.addTarget(self, action: #selector(ViewController.btnClick), for: UIControlEvents.touchUpInside)
+        
+        mConnectBtn.addTarget(self, action: #selector(ViewController.btnClick), for: UIControlEvents.touchUpInside)
+    }
     
-    @IBAction func mBtn(_ sender: Any) {
-        print("connect btn clicked")
+    @objc func btnClick(sender: UIButton?) {
+        let tag = sender?.tag
+        switch (tag!) {
+        case mConnectBtn.tag:
+            print("exec connect")
+            connectVpn()
+            break
+        case mStopBtn.tag:
+            print("exec stop connect")
+            self.manager?.connection.stopVPNTunnel()
+            break
+        default:
+            break
+        }
+    }
+    
+    func connectVpn () {
         let callback = { (error: Error?) -> Void in
             self.manager?.loadFromPreferences(completionHandler: { (error) in
                 guard error == nil else {
@@ -41,6 +84,7 @@ class ViewController: UIViewController {
                 
                 do {
                     try self.manager?.connection.startVPNTunnel(options: options)
+                    self.addVPNStatusObserver()
                 } catch {
                     print("\(error.localizedDescription)")
                 }
@@ -48,9 +92,60 @@ class ViewController: UIViewController {
         }
         
         configureVPN(callback: callback)
-        
     }
     
+    func addVPNStatusObserver() {
+        guard !observerAdded else{
+            return
+        }
+        loadProviderManager { [unowned self] (manager) -> Void in
+            if let manager = manager {
+                self.observerAdded = true
+                
+                NotificationCenter.default.addObserver(forName: NSNotification.Name.NEVPNStatusDidChange, object: manager.connection, queue: OperationQueue.main, using: { [unowned self] (notification) -> Void in
+                    self.updateVPNStatus(manager: manager)
+                })
+                
+            }
+        }
+    }
+    
+
+    
+    func updateVPNStatus(manager: NEVPNManager) {
+        switch manager.connection.status {
+        case .connected:
+            mVpnStatus.text = "connected"
+            print("connected")
+            self.vpnStatus = .On
+        case .connecting, .reasserting:
+            mVpnStatus.text = "connection"
+            print("connecting")
+            self.vpnStatus = .Connecting
+        case .disconnecting:
+            print("disconnecting")
+            mVpnStatus.text = "disconnecting"
+            self.vpnStatus = .Disconnecting
+        case .disconnected, .invalid:
+            print("off")
+            mVpnStatus.text = "off"
+            self.vpnStatus = .Off
+        }
+    }
+    
+
+    public func loadProviderManager(complete: @escaping (NETunnelProviderManager?) -> Void) {
+        NETunnelProviderManager.loadAllFromPreferences{ (managers, error) -> Void in
+            if let managers = managers {
+                if managers.count > 0 {
+                    let manager = managers[0]
+                    complete(manager)
+                    return
+                }
+            }
+            complete(nil)
+        }
+    }
 
 }
 
